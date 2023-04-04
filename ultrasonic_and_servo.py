e88b17464e30e7d22dbec856e7f07e8797d6b0c3
Evan Beaudoin <124371659+evan-beaudoin99@users.noreply.github.com>
2023-04-04 10:02:20 -0400

    Update ultrasonic_and_servo.py

--- ultrasonic_and_servo.py
+++ ultrasonic_and_servo.py
@@ -2,7 +2,7 @@
 """
 Created by: Evan Beaudoin
 Created on: March 2023
-Uses the HC-SR04 distance sensor to print the distance
+Uses the HC-SR04 distance sensor to move a servo
 """
 
 import time
@@ -10,7 +10,6 @@ import board
 import adafruit_hcsr04
 import pwmio
 from adafruit_motor import servo
-import threading
 
 
 pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
@@ -20,17 +19,35 @@ sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP13, echo_pin=board.GP14)
 
 
 def main() -> None:
+    # setup
+    train_coming = False
+    arm_up = True
+    
+    my_servo.angle = 180
+    print("starting")
+    time.sleep(1.0)
+    
+    
+    # loop
     while True:
         print((sonar.distance,))
         time.sleep(0.1)
         
         if sonar.distance < 10:
-            my_servo.angle = 180
-            # time.sleep(0.5)
+            train_coming = True
         else:
-            my_servo.angle = 0
-            # time.sleep(0.5)
+            train_coming = False
             
+        if arm_up == True and train_coming == True:
+            # move arm down
+            my_servo.angle = 0
+            time.sleep(1.5)
+            arm_up = False
+        elif arm_up == False and train_coming == False:
+            # move arm up
+            my_servo.angle = 180
+            time.sleep(1.5)
+            arm_up = True            
         
     
 if __name__ == "__main__":
